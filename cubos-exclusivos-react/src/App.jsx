5c19bbec1ab11993c5b066d8075214dbe1bc20b3
Miguel A. Alvarez <miguelkashir@gmail.com>
2020-12-22 17:48:27 +0100

    feat: develop Cube component

--- cubos-exclusivos-react/src/App.jsx
+++ cubos-exclusivos-react/src/App.jsx
@@ -1,15 +1,47 @@
-import './App.css';
-import t from './locales/es';
+import { useState } from 'react';
 
-const App = () => (
-  <div className="App">
-    <header>
-      <h1>{t.title}</h1>
-      <p>{t.subtitle.first}</p>
-      <p>{t.subtitle.second}</p>
-    </header>
-    <section>{/* cubes here */}</section>
-  </div>
-);
+import Cube from './components/Cube';
+import t from './locales/es';
+import './App.css';
+import imgGans356 from './assets/images/gans356.png';
+import img33x33x33 from './assets/images/33x33x33.png';
+import imgMasterPieceCube from './assets/images/masterpiececube.png';
+import imgVCubeMaze from './assets/images/v-cube-maze.png';
+
+const App = () => {
+  const [cubes, setCubes] = useState([
+    {
+      title: 'Gans356',
+      image: imgGans356
+    },
+    {
+      title: '33x33x33',
+      image: img33x33x33
+    },
+    {
+      title: 'Masterpiececube',
+      image: imgMasterPieceCube
+    },
+    {
+      title: 'V-Cube Maze',
+      image: imgVCubeMaze
+    }
+  ]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>{t.title}</h1>
+        <p>{t.subtitle.first}</p>
+        <p>{t.subtitle.second}</p>
+      </header>
+      <section>
+        {cubes.map(({ title, image }) => (
+          <Cube title={title} image={image} />
+        ))}
+      </section>
+    </div>
+  );
+};
 
 export default App;
--- cubos-exclusivos-react/src/locales/es.js
+++ cubos-exclusivos-react/src/locales/es.js
@@ -5,7 +5,8 @@ const t = {
       'La amplia oferta de cubos de rubiks se desarrolla a través de las más competentes manufacturas.',
     second:
       'En esta página podrás encontrar los cubos más exclusivos del mercado, ya sea por su eficacia o su elegancia.'
-  }
+  },
+  showMore: 'Ver más'
 };
 
 export default t;
